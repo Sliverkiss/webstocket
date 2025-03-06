1b57ea6e34c3b42938a05001368c46af086b7fba
Sliverkiss <1393579810@qq.com>
2025-03-06 20:32:12 +0800

    Update client.py

--- client.py
+++ client.py
@@ -22,29 +22,16 @@ def call():
     # 获取JSON请求体
     request_data = request.get_json()
 
-    # 判断必填参数 'appid' 和 'username' 是否存在
-    appid = request_data.get('appid')
-    username = request_data.get('username')
-
-    if not appid:
-        return jsonify({"error": "'appid' parameter is required"}), 400
-    if not username:
-        return jsonify({"error": "'username' parameter is required"}), 400
-
     # 获取其他动态参数（除了 appid 和 username）
-    kwargs = {key: value for key, value in request_data.items() if key not in ['appid', 'username']}
+    kwargs = {key: value for key, value in request_data.items()}
 
     # 调用 RpcCall 函数，传入动态的参数
-    result = RpcCall(
-        username=username,
-        appid=appid,
-        **kwargs
-    )
+    result = RpcCall(**kwargs)
 
     # 返回结果作为 JSON 响应
-    return jsonify(result)
+    return result
 
 
 if __name__ == '__main__':
     # Run the Flask app on port 5000
-    app.run(debug=True, host='0.0.0.0', port=12218)
+    app.run(debug=False, host='0.0.0.0', port=12218)
